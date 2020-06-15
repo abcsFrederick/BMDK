8157058b68f68ebbb340067f8763c6f1a17fcc81
Randy Johnson <johnsonra@mail.nih.gov>
2020-06-15 13:10:54 -0400

    Moving example data to inst/extdata
    In order to include raw example data in the package, we'll include it in inst/extdata.

--- tests/testthat/test-read_bmdk.R
+++ tests/testthat/test-read_bmdk.R
@@ -2,7 +2,7 @@ context("test-read_bmdk")
 
 test_that("read_bmdk returns a valid object", {
     
-    tst <- read_bmdk('file path goes here')
+    tst <- read_bmdk(system.file('extdata', 'BMDK_test.txt', package = 'BMDK'))
     
     expect_is(tst, 'data.frame')
     
